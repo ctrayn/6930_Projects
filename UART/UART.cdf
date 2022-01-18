19a9854422cbc611d37259879a5bbefa5bde5ed5
jonahboe <boe12005@byui.edu>
2022-01-18 14:34:35 -0700

    Added my_UART code. Not Working

---
+++ UART/UART.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 20.1.1 Build 720 11/11/2020 SJ Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(10M50DAF484) Path("//Mac/Home/Documents/School/Graduate/Classes/2022 - Spring/ECE 6930 - Reconfig/6930_Projects/UART/") File("UART.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
--- UART/UART.vhd
+++ UART/UART.vhd
@@ -9,9 +9,9 @@ entity UART is
 		KEY : in std_logic_vector(1 downto 0);
 		
 		--OUTPUT
-		RX : out std_logic;			--Connected to pin 40 on J1 (white wire)
-		Tx : in  std_logic 			--Connected to pin 39 on J1 (green wire)
-	);										--   Ground is pin 30 on J1 (black wire); Leave power disconnected (red wire)
+		RX : in  std_logic;			--Connected to pin 40 on J1 (white wire)
+		Tx : out std_logic 			--Connected to pin 39 on J1 (green wire)
+	);										--Ground is pin 30 on J1 (black wire); Leave power disconnected (red wire)
 end entity UART;
 
 architecture behavioral of UART is
@@ -23,51 +23,36 @@ architecture behavioral of UART is
 	signal data_tx, data_rx : unsigned(7 downto 0);
 	signal pll_clk, pll_lock : std_logic;
 	
+	constant clk_div : unsigned(15 downto 0) := x"0208"; -- This is based on our clk being 10Mhz 10M/19200
+	
 	constant A_upper : unsigned(7 downto 0) := X"41";
 	constant Z_upper : unsigned(7 downto 0) := X"5A";
 	constant a_lower : unsigned(7 downto 0) := X"61";
 	constant z_lower : unsigned(7 downto 0) := X"7A";
 	constant twenty  : unsigned(7 downto 0) := X"14";
 	
-	component baud_pll
-		port
-		(
-			areset		: IN STD_LOGIC  := '0';
-			inclk0		: IN STD_LOGIC  := '0';
-			c0				: OUT STD_LOGIC ;
-			locked		: OUT STD_LOGIC
-		);
-	end component;
-	
 	component my_UART
 		port
 		(
-			clk   : in std_logic;			--Assuming this would be the Baud Rate
-			rst_l : in std_logic;			--Direct from Key inputs
-			TX : in  std_logic;
-			RX : out std_logic;
-			TX_flag : in std_logic;
-			RX_flag : out std_logic;
-			data_tx : in  unsigned(7 downto 0);
-			data_rx : out unsigned(7 downto 0)
+			clk   	: in 	std_logic;
+			clk_div 	: in  unsigned(15 downto 0);
+			rst_l 	: in 	std_logic;
+			TX 		: out std_logic;
+			RX 		: in 	std_logic;
+			TX_flag 	: in 	std_logic;
+			RX_flag 	: out std_logic;
+			data_tx 	: in  unsigned(7 downto 0);
+			data_rx 	: out unsigned(7 downto 0)
 		);
 	end component;	
 	
 begin
-
-	put : baud_pll
-		port map
-		(
-			areset => KEY(0),
-			inclk0 => ADC_CLK_10,
-			c0 => pll_clk,
-			locked => pll_lock
-		);
 		
 	dut : my_UART
 		port map
 		(
 			clk => ADC_CLK_10,
+			clk_div => clk_div,
 			rst_l => KEY(0),
 			TX => TX,
 			RX => RX,
@@ -80,8 +65,8 @@ begin
 	process (ADC_CLK_10, KEY(0), RX_flag) begin
 		if (KEY(0) = '0') then
 			state <= START;
-			TX_flag <= '0';
-			data_tx <= (others => '0');
+			TX_flag <= '1';
+			data_tx <= A_upper;
 		else
 			if rising_edge(ADC_CLK_10) then
 				case state is
@@ -99,6 +84,8 @@ begin
 							data_tx <= data_rx + twenty;
 						elsif ((data_rx >= a_lower) and (data_rx <= z_lower)) then
 							data_tx <= data_rx - twenty;
+						else
+							data_tx <= x"45";
 						end if;
 						state <= START;
 						TX_flag <= '1';
