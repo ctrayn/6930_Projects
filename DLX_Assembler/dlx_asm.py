f204de32f4a59e3b2b69d540d01f5b9db7a0f50a
Calvin Passmore <calvin.passm@gmail.com>
2022-01-21 14:36:37 -0700

    Intermittant commit

--- DLX_Assembler/dlx_asm.py
+++ DLX_Assembler/dlx_asm.py
@@ -35,6 +35,7 @@ data_file.write("\n")
 
 line = source_file.readline()
 i = 0
+variable = {}
 while line != "\t.text\n":
     if (';' in line) or (line == "\t.data\n") or (line == "\n") or (line == " \n") or (line == "\t\n"):  # Skip lines
         pass
@@ -50,7 +51,8 @@ while line != "\t.text\n":
             print(f"Error: {data[1]} != {len(values)}")
             sys.exit(1)
         for index in range(len(values)):
-            data_file.write(f"{i:>03X} : {int(values[index]):>08X}; --{data[0]}[{index}]\n")
+            data_file.write(f"{i:>03X} : {int(values[index]):>08X};\t--{data[0]}[{index}]\n")
+            variable[data[0]] = i   # Save the addresses for reference in the code
             i += 1
 
     line = source_file.readline()
@@ -70,127 +72,139 @@ code_file.write("\n")
 
 i = 0
 labels = {}
+current_pos = source_file.tell()
+
 while line != '':
     if (';' in line) or (line == "\t.text\n") or (line == "\n") or (line == " \n") or (line == "\t\n") \
             or (line == "\t\t\n") or (line == "\t"):  # Skip lines
         pass
     else:
         line = line.strip("\n")
-        if line[0] == "\t":
-            items = line.split("\t")
-            while '' in items:      # Remove blank entries
-                items.remove('')
+        items = line.split("\t")
+        while '' in items:  # Remove blank entries
+            items.remove('')
 
-            # This is where each OPCODE is going to be evaluated
-            word = ""
-            if items[0] == "NOP":
-                word = "0" * 32
-            # TODO: OPCODEs below HERE still need to be evaluated
-            elif items[0] == "LW":
-                word = f"{1:>06X}"
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SW":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "ADD":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "ADDI":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "ADDU":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "ADDUI":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SUB":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SUBI":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SUBU":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SUBUI":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "AND":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "ANDI":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "OR":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "ORI":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "XOR":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "XORI":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SLL":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SLLI":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SRL":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SRLI":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SRA":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SRAI":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SLT":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SLTI":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SLTU":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SLTUI":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SGT":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SGTI":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SGTU":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SGTUI":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SLE":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SLEI":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SLEU":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SLEUI":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SGE":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SGEI":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SGEU":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SGEUI":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SEQ":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SEQI":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SNE":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "SNEI":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "BEQZ":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "BNEZ":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "J":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "JR":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "JAL":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            elif items[0] == "JALR":
-                print(f"\033[31m Need to write code for {items[0]} \033[0m")
-            else:
-                print(f"\033[31m Error: OPCODE {items[0]} unknown \033[0m")
 
-            print(f"{i:>03X} {items}")
-            i += 1
+
+    line = source_file.readline()
+
+source_file.seek(current_pos)       # Reset to the position saved earlier
+while line != '':
+    if (';' in line) or (line == "\t.text\n") or (line == "\n") or (line == " \n") or (line == "\t\n") \
+            or (line == "\t\t\n") or (line == "\t"):  # Skip lines
+        pass
+    else:
+        line = line.strip("\n")
+        items = line.split("\t")
+        while '' in items:      # Remove blank entries
+            items.remove('')
+
+        # This is where each OPCODE is going to be evaluated
+        word = ""
+        if items[0] == "NOP":
+            word = "0" * 32
+        # TODO: OPCODEs below HERE still need to be evaluated
+        elif items[0] == "LW":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SW":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "ADD":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "ADDI":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "ADDU":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "ADDUI":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SUB":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SUBI":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SUBU":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SUBUI":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "AND":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "ANDI":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "OR":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "ORI":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "XOR":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "XORI":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SLL":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SLLI":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SRL":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SRLI":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SRA":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SRAI":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SLT":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SLTI":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SLTU":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SLTUI":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SGT":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SGTI":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SGTU":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SGTUI":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SLE":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SLEI":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SLEU":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SLEUI":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SGE":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SGEI":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SGEU":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SGEUI":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SEQ":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SEQI":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SNE":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "SNEI":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "BEQZ":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "BNEZ":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "J":
+            word = "101101" + f"{labels.get(items[1]):>025b}"  # This is in binary
+        elif items[0] == "JR":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "JAL":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
+        elif items[0] == "JALR":
+            print(f"\033[31m Need to write code for {items[0]} \033[0m")
         else:
-            labels[line] = i + 1            # This will set up a place we can reference absolute addresses
+            print(f"\033[31m Error: OPCODE {items[0]} unknown \033[0m")
 
+        print(f"{i:>03X} {items}")
+        i += 1
     line = source_file.readline()
 
 code_file.write("\nEND;\n")
