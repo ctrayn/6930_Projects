1aae1267f03462c18a3341be3ca40d5da6135be2
jonahboe <boe12005@byui.edu>
2022-02-09 11:36:51 -0700

    Working out kinks

---
+++ DLX/Common.vhd
@@ -0,0 +1,81 @@
+---------------------------------------------------------
+-- DLX Processor
+--
+-- Jonah Boe
+-- Calvin Passmore
+-- Utah State University
+-- ECE 6930, Spring 2022
+---------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+
+-- Our states need to be accesable to everyone
+package common is
+	--Constants
+	constant OP_NOP	: std_logic_vector(5 downto 0) := B"000000";
+	constant OP_LW		: std_logic_vector(5 downto 0) := B"000001";
+	constant OP_SW		: std_logic_vector(5 downto 0) := B"000010";
+	constant OP_ADD	: std_logic_vector(5 downto 0) := B"000011";
+	constant OP_ADDI	: std_logic_vector(5 downto 0) := B"000100";
+	constant OP_ADDU	: std_logic_vector(5 downto 0) := B"000101";
+	constant OP_ADDUI	: std_logic_vector(5 downto 0) := B"000110";
+	constant OP_SUB	: std_logic_vector(5 downto 0) := B"000111";
+	constant OP_SUBI	: std_logic_vector(5 downto 0) := B"001000";
+	constant OP_SUBU	: std_logic_vector(5 downto 0) := B"001001";
+	constant OP_SUBUI	: std_logic_vector(5 downto 0) := B"001010";
+	constant OP_AND	: std_logic_vector(5 downto 0) := B"001011";
+	constant OP_ANDI	: std_logic_vector(5 downto 0) := B"001100";
+	constant OP_OR		: std_logic_vector(5 downto 0) := B"001101";
+	constant OP_ORI	: std_logic_vector(5 downto 0) := B"001110";
+	constant OP_XOR	: std_logic_vector(5 downto 0) := B"001111";
+	constant OP_XORI	: std_logic_vector(5 downto 0) := B"010000";
+	constant OP_SLL	: std_logic_vector(5 downto 0) := B"010001";
+	constant OP_SLLI	: std_logic_vector(5 downto 0) := B"010010";
+	constant OP_SRL	: std_logic_vector(5 downto 0) := B"010011";
+	constant OP_SRLI	: std_logic_vector(5 downto 0) := B"010100";
+	constant OP_SRA	: std_logic_vector(5 downto 0) := B"010101";
+	constant OP_SRAI	: std_logic_vector(5 downto 0) := B"010110";
+	constant OP_SLT	: std_logic_vector(5 downto 0) := B"010111";
+	constant OP_SLTI	: std_logic_vector(5 downto 0) := B"011000";
+	constant OP_SLTU	: std_logic_vector(5 downto 0) := B"011001";
+	constant OP_SLTUI	: std_logic_vector(5 downto 0) := B"011010";
+	constant OP_SGT	: std_logic_vector(5 downto 0) := B"011011";
+	constant OP_SGTI	: std_logic_vector(5 downto 0) := B"011100";
+	constant OP_SGTU	: std_logic_vector(5 downto 0) := B"011101";
+	constant OP_SGTUI	: std_logic_vector(5 downto 0) := B"011110";
+	constant OP_SLE	: std_logic_vector(5 downto 0) := B"011111";
+	constant OP_SLEI	: std_logic_vector(5 downto 0) := B"100000";
+	constant OP_SLEU	: std_logic_vector(5 downto 0) := B"100001";
+	constant OP_SLEUI	: std_logic_vector(5 downto 0) := B"100010";
+	constant OP_SGE	: std_logic_vector(5 downto 0) := B"100011";
+	constant OP_SGEI	: std_logic_vector(5 downto 0) := B"100100";
+	constant OP_SGEU	: std_logic_vector(5 downto 0) := B"100101";
+	constant OP_SGEUI	: std_logic_vector(5 downto 0) := B"100110";	
+	constant OP_SEQ	: std_logic_vector(5 downto 0) := B"100111";
+	constant OP_SEQI	: std_logic_vector(5 downto 0) := B"101000";
+	constant OP_SNE	: std_logic_vector(5 downto 0) := B"101001";
+	constant OP_SNEI	: std_logic_vector(5 downto 0) := B"101010";
+	constant OP_BEQZ	: std_logic_vector(5 downto 0) := B"101011";
+	constant OP_BNEZ	: std_logic_vector(5 downto 0) := B"101100";
+	constant OP_J		: std_logic_vector(5 downto 0) := B"101101";
+	constant OP_JR		: std_logic_vector(5 downto 0) := B"101110";
+	constant OP_JAL	: std_logic_vector(5 downto 0) := B"101111";
+	constant OP_JALR	: std_logic_vector(5 downto 0) := B"110000";
+	
+	-- Function 1: returns '1' if function is a store function
+	function OpIsWriteBack (op : std_logic_vector(5 downto 0)) return std_logic;
+end common;
+
+package body common is
+	-- Function 1 body: returns '1' if function is a store function
+   function OpIsWriteBack (op : std_logic_vector(5 downto 0)) return std_logic is
+	begin
+		if unsigned(op) = B"000001" or (unsigned(op) >= X"000011" and unsigned(op) <= X"101010") then
+			return '1';
+		else
+			return '0';
+		end if;
+	end OpIsWriteBack;
+end common;
---
+++ DLX/Decode.vhd
@@ -0,0 +1,115 @@
+---------------------------------------------------------
+-- DLX Processor
+--
+-- Jonah Boe
+-- Calvin Passmore
+-- Utah State University
+-- ECE 6930, Spring 2022
+---------------------------------------------------------
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use work.common.all;
+
+entity Decode is
+	port (
+		--INPUT 
+		clk					: in std_logic;
+		rst_l					: in std_logic;
+		pc_in					: in std_logic_vector(9  downto 0);
+		instruction_in		: in std_logic_vector(31 downto 0);
+		w_instruction		: in std_logic_vector(4  downto 0);			--Write address
+		w_data				: in std_logic_vector(31 downto 0);			--Write data
+		--OUTPUT
+		Imm					: out std_logic_vector(31 downto 0);		--Immediate value
+		pc_out				: out std_logic_vector(9  downto 0);		--Program counter, delayed by 1 cycle
+		instruction_out	: out std_logic_vector(31 downto 0);		--The instruction, delayed by 1 cycle
+		RS1					: out std_logic_vector(31 downto 0);		--The data from RS1
+		RS2					: out std_logic_vector(31 downto 0)			--The data from RS2
+	);
+end entity Decode;
+
+architecture behavioral of Decode is
+	
+	-- Build a 2-D array type for the RAM
+	subtype reg_t is std_logic_vector(31 downto 0);
+	type memory_t is array(31 downto 0) of reg_t;
+	
+	-- Declare the RAM signal.
+	signal ram 		: memory_t;
+	signal opcode 	: std_logic_vector(5  downto 0);
+	signal rd		: natural;
+	signal r1		: natural;
+	signal r2		: natural;
+	signal im_val	: std_logic_vector(15 downto 0);
+	
+begin
+
+	--rename some parts of the input instruction
+	opcode 	<= instruction_in(31 downto 26);
+	rd	   	<= to_integer(unsigned(instruction_in(25 downto 21)));
+	r1			<= to_integer(unsigned(instruction_in(20 downto 16)));
+	r2 		<= to_integer(unsigned(instruction_in(15 downto 11)));
+	im_val 	<= instruction_in(15 downto 0);
+	
+	--Signals that just get delayed and passed on
+	process(clk) begin
+		if rising_edge(clk) then
+			pc_out <= pc_in;
+			instruction_out <= instruction_in;
+		end if;
+	end process;
+	
+	--Write
+	process(clk) begin
+		if rising_edge(clk) and opIsWriteBack(w_instruction(31 downto 26)) = '1' and w_instruction(25 downto 21) /= B"00000" then
+			ram(to_integer(unsigned(w_instruction(25 downto 21)))) <= w_data;
+		else
+			ram(0) <= X"00000000";
+		end if;
+	end process;
+	
+	--Read
+	process(clk) begin
+		if rising_edge(clk) then
+			if opcode = OP_NOP or opcode = OP_J or opcode = OP_JAL then
+				RS1 <= (others => '0');
+				RS2 <= (others => '0');
+				Imm <= (others => '0');
+			elsif opcode = OP_LW or opcode = OP_SW then
+				RS1 <= ram(r1);
+				RS2 <= ram(rd);
+				Imm(15 downto 0) <= im_val;
+				Imm(31 downto 16) <= (others => '0');	
+			elsif opcode = OP_JR or opcode = OP_JALR or opcode = OP_BEQZ or opcode = OP_BNEZ then
+				RS1 <= ram(r1);
+				RS2 <= (others => '0');
+				Imm <= (others => '0');
+			elsif opcode = OP_ADDI or opcode = OP_SUBI or opcode = OP_SLTI or opcode = OP_SGTI or opcode = OP_SLEI or opcode = OP_SGEI or
+					opcode = OP_SEQI or opcode = OP_SNEI then
+				RS1 <= ram(r1);
+				RS2 <= (others => '0');
+				Imm(15 downto 0) <= im_val;
+				Imm(31 downto 16) <= (others => im_val(15));	
+			elsif opcode = OP_ADDUI or opcode = OP_SUBUI or opcode = OP_ANDI or opcode = OP_ORI or opcode = OP_XORI or opcode = OP_SLLI or
+					opcode = OP_SRLI or opcode = OP_SRAI or opcode = OP_SLTUI or opcode =OP_SGTUI or opcode = OP_SLEUI or opcode = OP_SGEUI then
+				RS1 <= ram(r1);
+				RS2 <= (others => '0');
+				Imm(15 downto 0) <= im_val;
+				Imm(31 downto 16) <= (others => '0');		
+			elsif opcode = OP_ADD or opcode = OP_ADDU or opcode = OP_SUB or opcode = OP_SUBU or opcode = OP_AND or opcode = OP_OR or
+					opcode = OP_XOR or opcode = OP_SLL or opcode = OP_SRL or opcode = OP_SRA or opcode = OP_SLT or opcode = OP_SLTU or 
+					opcode = OP_SGT or opcode = OP_SGTU or opcode = OP_SLE or opcode = OP_SLEU or opcode = OP_SGE or opcode = OP_SGEU or
+					opcode = OP_SEQ or opcode = OP_SNE then
+				RS1 <= ram(r1);
+				RS2 <= ram(r2);
+				Imm <= (others => '0');
+			else
+				RS1 <= (others => '0');
+				RS2 <= (others => '0');
+				Imm <= (others => '0');
+			end if;
+		end if;
+	end process;
+
+end architecture behavioral;
