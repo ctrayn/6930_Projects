6f50f2dee99b2de50268a20c0d1908d72f4f27ae
jonahboe <boe12005@byui.edu>
2022-02-09 12:21:30 -0700

    Reworking, opcodes missing.

--- DLX/Decode.vhd
+++ DLX/Decode.vhd
@@ -13,7 +13,7 @@ use work.common.all;
 
 entity Decode is
 	port (
-		--INPUT 
+		--INPUT
 		clk					: in std_logic;
 		rst_l					: in std_logic;
 		pc_in					: in std_logic_vector(9  downto 0);
@@ -30,11 +30,11 @@ entity Decode is
 end entity Decode;
 
 architecture behavioral of Decode is
-	
+
 	-- Build a 2-D array type for the RAM
 	subtype reg_t is std_logic_vector(31 downto 0);
 	type memory_t is array(31 downto 0) of reg_t;
-	
+
 	-- Declare the RAM signal.
 	signal ram 		: memory_t;
 	signal opcode 	: std_logic_vector(5  downto 0);
@@ -42,7 +42,7 @@ architecture behavioral of Decode is
 	signal r1		: natural;
 	signal r2		: natural;
 	signal im_val	: std_logic_vector(15 downto 0);
-	
+
 begin
 
 	--rename some parts of the input instruction
@@ -51,7 +51,7 @@ begin
 	r1			<= to_integer(unsigned(instruction_in(20 downto 16)));
 	r2 		<= to_integer(unsigned(instruction_in(15 downto 11)));
 	im_val 	<= instruction_in(15 downto 0);
-	
+
 	--Signals that just get delayed and passed on
 	process(clk) begin
 		if rising_edge(clk) then
@@ -59,7 +59,7 @@ begin
 			instruction_out <= instruction_in;
 		end if;
 	end process;
-	
+
 	--Write
 	process(clk) begin
 		if rising_edge(clk) and opIsWriteBack(w_instruction(31 downto 26)) = '1' and w_instruction(25 downto 21) /= B"00000" then
@@ -68,47 +68,112 @@ begin
 			ram(0) <= X"00000000";
 		end if;
 	end process;
-	
+
 	--Read
 	process(clk) begin
 		if rising_edge(clk) then
-			if opcode = OP_NOP or opcode = OP_J or opcode = OP_JAL then
-				RS1 <= (others => '0');
-				RS2 <= (others => '0');
-				Imm <= (others => '0');
-			elsif opcode = OP_LW or opcode = OP_SW then
-				RS1 <= ram(r1);
-				RS2 <= ram(rd);
-				Imm(15 downto 0) <= im_val;
-				Imm(31 downto 16) <= (others => '0');	
-			elsif opcode = OP_JR or opcode = OP_JALR or opcode = OP_BEQZ or opcode = OP_BNEZ then
-				RS1 <= ram(r1);
-				RS2 <= (others => '0');
-				Imm <= (others => '0');
-			elsif opcode = OP_ADDI or opcode = OP_SUBI or opcode = OP_SLTI or opcode = OP_SGTI or opcode = OP_SLEI or opcode = OP_SGEI or
-					opcode = OP_SEQI or opcode = OP_SNEI then
-				RS1 <= ram(r1);
-				RS2 <= (others => '0');
-				Imm(15 downto 0) <= im_val;
-				Imm(31 downto 16) <= (others => im_val(15));	
-			elsif opcode = OP_ADDUI or opcode = OP_SUBUI or opcode = OP_ANDI or opcode = OP_ORI or opcode = OP_XORI or opcode = OP_SLLI or
-					opcode = OP_SRLI or opcode = OP_SRAI or opcode = OP_SLTUI or opcode =OP_SGTUI or opcode = OP_SLEUI or opcode = OP_SGEUI then
-				RS1 <= ram(r1);
-				RS2 <= (others => '0');
-				Imm(15 downto 0) <= im_val;
-				Imm(31 downto 16) <= (others => '0');		
-			elsif opcode = OP_ADD or opcode = OP_ADDU or opcode = OP_SUB or opcode = OP_SUBU or opcode = OP_AND or opcode = OP_OR or
-					opcode = OP_XOR or opcode = OP_SLL or opcode = OP_SRL or opcode = OP_SRA or opcode = OP_SLT or opcode = OP_SLTU or 
-					opcode = OP_SGT or opcode = OP_SGTU or opcode = OP_SLE or opcode = OP_SLEU or opcode = OP_SGE or opcode = OP_SGEU or
-					opcode = OP_SEQ or opcode = OP_SNE then
-				RS1 <= ram(r1);
-				RS2 <= ram(r2);
-				Imm <= (others => '0');
-			else
-				RS1 <= (others => '0');
-				RS2 <= (others => '0');
-				Imm <= (others => '0');
-			end if;
+			case opcode is
+				when OP_NOP =>
+					RS1 <= (others => '0');
+					RS2 <= (others => '0');
+					Imm <= (others => '0');
+
+				when OP_LW | OP_SW =>
+					RS1 <= ram(rd);
+					RS2 <= ram(r1);
+					Imm(31 downto 16) <= (others => '0');
+					Imm(15 downto 0)  <= im_val;
+
+				when OP_JR | OP_JALR =>
+					RS1 <= ram(rd);
+					RS2 <= (others => '0');
+					Imm <= (others => '0');
+
+				when OP_J | OP_JAL =>
+					RS1 <= (others => '0');
+					RS2 <= (others => '0');
+					Imm(31 downto 26) <= (others => '0');
+					Imm(25 downto 0) <= instruction_in(25 downto 0);
+
+				when OP_BEQZ | OP_BNEZ =>
+					RS1 <= ram(rd);
+					RS2 <= (others => '0');
+					Imm(31 downto 21) <= (others => '0');
+					Imm(20 downto 0) <= instruction_in(20 downto 0);
+
+				when OP_ADD | OP_ADDU | OP_SUB | OP_SUBU =>
+					RS1 <= ram(r1);
+					RS2 <= ram(r2);
+					Imm <= (others => '0');
+
+				when OP_ADDI | OP_SUBI =>
+					RS1 <= ram(r1);
+					RS2 <= (others => '0');
+					Imm(31 downto 16) <= (others => im_val(15));		--sign extend
+					Imm(15 downto 0) <= im_val;
+
+				when OP_ADDUI | OP_SUBUI =>
+					RS1 <= ram(r1);
+					RS2 <= (others => '0');
+					Imm(31 downto 16) <= (others => '0');		--sign extend
+					Imm(15 downto 0) <= im_val;
+
+				when OP_AND | OP_OR | OP_XOR =>
+					RS1 <= ram(r1);
+					RS2 <= ram(r2);
+					Imm <= (others => '0');
+
+				when OP_ANDI | OP_ORI | OP_XORI =>
+					RS1 <= ram(r1);
+					RS2 <= (others => '0');
+					Imm(16 downto 0) <= (others => '0');
+					Imm(15 downto 0) <= im_val;
+
+				when OP_SLL | OP_SRL | OP_SRA =>
+					RS1 <= ram(r1);
+					RS2 <= ram(r2);
+					Imm <= (others => '0');
+
+				when OP_SLLI | OP_SRLI | OP_SRAI =>
+					RS1 <= ram(r1);
+					RS2 <= ram(r2);
+					Imm <= (others => '0');
+
+				when OP_SLT | OP_SLTU | OP_SGT | OP_SGTU =>
+					RS1 <= ram(r1);
+					RS2 <= ram(r2);
+					Imm <= (others => '0');
+
+				when OP_SLTI | OP_SGTI  =>
+					RS1 <= ram(r1);
+					RS2 <= (others => '0');
+					Imm(31 downto 16) <= (others => im_val(15));		--sign extend
+					Imm(15 downto 0) <= im_val;
+
+				when OP_SLTUI | OP_SGTUI =>
+					RS1 <= ram(r1);
+					RS2 <= (others => '0');
+					Imm(15 downto 0) <= im_val;
+					Imm(16 downto 0) <= (others => '0');
+
+				when OP_SLE | OP_SLEU | OP_SGE | OP_SGEU | OP_SEQ | OP_SNE =>
+					RS1 <= ram(r1);
+					RS2 <= ram(r2);
+					Imm <= (others => '0');
+
+				when OP_SLEI | OP_SGEI | OP_SEQI | OP_SNEI =>
+					RS1 <= ram(r1);
+					RS2 <= (others => '0');
+					Imm(15 downto 0) <= im_val;
+					Imm(31 downto 16) <= (others => im_val(15));		--sign extend
+
+				when OP_SLEUI | OP_SGEUI =>
+					RS1 <= ram(r1);
+					RS2 <= (others => '0');
+					Imm(15 downto 0) <= im_val;
+					Imm(16 downto 0) <= (others => '0');
+
+			end case;
 		end if;
 	end process;
 
